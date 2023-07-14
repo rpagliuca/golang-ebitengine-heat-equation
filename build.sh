f38d18bc8fe1ed5c99900997628db67c0cf7e92f
Rafael Joseph Pagliuca dos Santos <rafael.santos.193@gmail.com>
2023-07-14 01:31:39 -0300

    feat: Move web/ to docs/

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-env GOOS=js GOARCH=wasm go build -o web/heat.wasm app/cmd/run
-cp $(go env GOROOT)/misc/wasm/wasm_exec.js web/
-npx http-server web/
+env GOOS=js GOARCH=wasm go build -o docs/heat.wasm app/cmd/run
+cp $(go env GOROOT)/misc/wasm/wasm_exec.js docs/
+npx http-server docs/
